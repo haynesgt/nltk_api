6f00090a20df29ab00c1a22a84ba111e0cebd988
Gavin Haynes <haynes.gt@gmail.com>
2021-12-27 23:00:52 +0000

    feat: add tokenize endpoints

--- nltk-api/__init__.py
+++ nltk-api/__init__.py
@@ -1,10 +1,36 @@
 from fastapi import FastAPI
-
 import nltk
+
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-        return {"message": "Ok"}
+        return {"message": "Ok. See docs at /docs"}
+
+class SentTokenizeRequest(BaseModel):
+    text: str
+
+class SentTokenizeResponse(BaseModel):
+    tokens: List[str]
+
+@app.post("/sent_tokenize")
+async def sent_tokenize(req: SentTokenizeRequest):
+    tokens = nltk.sent_tokenize(req.text)
+    response = SentTokenizeResponse(tokens=tokens)
+    return response
+
+class WordTokenizeRequest(BaseModel):
+    text: str
+
+class WordTokenizeResponse(BaseModel):
+    tokens: List[str]
+
+@app.post("/word_tokenize")
+async def word_tokenize(req: WordTokenizeRequest):
+    tokens = nltk.word_tokenize(req.text)
+    response = WordTokenizeResponse(tokens=tokens)
+    return response
 
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+PORT=${PORT-8080}
+gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 --worker-class uvicorn.workers.UvicornWorker --reload nltk-api:app 
